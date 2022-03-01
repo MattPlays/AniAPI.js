46d1bec2234065b0ce67012801b1e5fef6495216
Artrix <39530102+Artrix9095@users.noreply.github.com>
2022-03-01 13:30:14 -0800

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-const { API } = require('./dist/index');
-(async () => {
-    const api = new API(
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEyNDkiLCJuYmYiOjE2NDU3NDUzODAsImV4cCI6MTY0ODMzNzM4MCwiaWF0IjoxNjQ1NzQ1MzgwfQ.olbs8OSb-8NW_ZWQagUXOCReH-wcnry8jTL9qtPQi1A'
-    );
-
-    console.log(await api.Anime.Random(2));
-})();
