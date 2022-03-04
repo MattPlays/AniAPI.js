bf977915157c7631bc24bdf375daf121b405d1ec
Artrix <39530102+Artrix9095@users.noreply.github.com>
2022-03-04 08:53:45 -0800

    add custom headers in default headers

--- src/constants.ts
+++ src/constants.ts
@@ -2,8 +2,12 @@ export const API_URL = 'https://api.aniapi.com';
 
 export const API_VERSION = 1;
 
-export const DEFAULT_HEADERS = (jwt: string) => ({
+export const DEFAULT_HEADERS = (
+    jwt: string,
+    headers: { [key: string]: string } = {}
+) => ({
     Authorization: `Bearer ${jwt}`,
     'Content-Type': 'application/json',
     Accept: 'application/json',
+    ...headers,
 });
